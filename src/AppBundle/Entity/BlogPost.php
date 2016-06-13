e9d779b3f16b5d2ec47ecdf7d6addf62ad51bf23
lichuang <lichuang@baijiahulian.com>
2016-06-13 20:49:52 +0800

    pv default 0

--- src/AppBundle/Entity/BlogPost.php
+++ src/AppBundle/Entity/BlogPost.php
@@ -12,6 +12,11 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class BlogPost
 {
+    function __construct()
+    {
+        $this->pv = 0;
+    }
+
     /**
      * @ORM\ManyToOne(targetEntity="Category", inversedBy="blogPosts")
      */
