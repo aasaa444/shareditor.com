63f8e661acb640bac918ff7cea4a1f3d99b7a6e0
lichuang <lichuang@baijiahulian.com>
2016-09-26 09:00:43 +0800

    xiaoertu

--- vendor/egeloen/ckeditor-bundle/Resources/public/config.js
+++ vendor/egeloen/ckeditor-bundle/Resources/public/config.js
@@ -7,7 +7,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-    config.extraPlugins = 'codesnippet';
+    config.extraPlugins = 'codesnippet,bgimage';
     config.codeSnippet_theme = 'ir_black';
     config.codeSnippet_languages = {
         xml: 'XML',
